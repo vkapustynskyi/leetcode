cc206a94baf440deef7f543fa705d981da89a8ff
Volodymyr Kapustynskyi <volodymyr.kapustynskyi@orientedsoft.com>
2024-10-18 19:50:46 +0200

    BestTimeToBuyAndSellStock working solution

---
+++ src/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,23 @@
+
+public class BestTimeToBuyAndSellStock {
+    public static void main(String[] args) {
+        int[] nums = new int[]{7, 1, 5, 3, 6, 4};
+
+        int k = maxProfit(nums);
+        System.out.println(k);
+    }
+
+    public static int maxProfit(int[] prices) {
+        int profit = 0;
+        int min = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < min) {
+                min = prices[i];
+            }
+            if (prices[i] - min > profit) {
+                profit = prices[i] - min;
+            }
+        }
+        return profit;
+    }
+}
