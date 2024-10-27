3ab41ca2578c1843dc454c254b614aad900a75d0
Volodymyr Kapustynskyi <volodymyr.kapustynskyi@orientedsoft.com>
2024-10-27 16:28:42 +0100

    JumpGame solution

---
+++ src/JumpGame.java
@@ -0,0 +1,23 @@
+public class JumpGame {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 3, 1, 1, 4};
+
+        boolean result = Solution.canJump(nums);
+
+        System.out.println(result);
+    }
+
+    private static class Solution {
+        public static boolean canJump(int[] nums) {
+            int indexToReach =  nums.length - 1;
+            for (int i = nums.length - 2; i >= 0; i--) {
+                if (nums[i] + i >= indexToReach) {
+                    indexToReach = i;
+                }
+            }
+            return indexToReach == 0;
+        }
+    }
+
+}
